422363b489022928e54e58aee24ff5bccf8f4104
daramedeiros <daraamedeiros@outlook.com>
2019-10-08 22:17:54 -0300

    continuação da construção da api - aula 02

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -9,4 +9,19 @@ exports.getById = (require, response) => {
     const id = require.params.id
     console.log(id)
     response.status(200).send(alunas.find(aluna => aluna.id == id))
+
 }
+
+exports.getBooks = (req, resp) => {
+    const id = req.params.id
+    const aluna = alunas.find(aluna => aluna.id == id)
+    console.log(aluna)
+
+    const livrosAlunas = aluna.livros
+    const tituloLivros = livrosAlunas.map(livro => livro.titulo)
+
+    resp.status(200).send(tituloLivros)
+}
+
+
+// http://braziljs.github.io/eloquente-javascript/
